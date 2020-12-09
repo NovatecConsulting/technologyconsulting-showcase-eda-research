be6353b620eb3cc3ea75ef4e991b5046c62eb6ed
Tobias Bohn <tobiasbohnxz@gmail.com>
2020-12-09 13:29:04 +0100

    Created Function Identities, whitelist them in keyvault

--- modules/keyVault.tf
+++ modules/keyVault.tf
@@ -23,7 +23,7 @@ resource "azurerm_key_vault" "key_vault" {
 }
 
 
-# Producer FaaS 
+# Customer Producer FaaS 
 resource "azurerm_key_vault_access_policy" "key_vault_producer_function_access_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id = data.azurerm_client_config.current.tenant_id
@@ -32,7 +32,7 @@ resource "azurerm_key_vault_access_policy" "key_vault_producer_function_access_p
   secret_permissions = ["delete", "get", "set", "list"]
 }
 
-# Consumer FaaS 
+# Customer Consumer FaaS 
 resource "azurerm_key_vault_access_policy" "key_vault_consumer_function_access_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id = data.azurerm_client_config.current.tenant_id
@@ -40,3 +40,22 @@ resource "azurerm_key_vault_access_policy" "key_vault_consumer_function_access_p
 
   secret_permissions = ["delete", "get", "set", "list"]
 }
+
+# Order Producer FaaS 
+resource "azurerm_key_vault_access_policy" "key_vault_producer_function_access_policy" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_function_app.order_function_producer.identity[0].principal_id
+
+  secret_permissions = ["delete", "get", "set", "list"]
+}
+
+# Order Consumer FaaS 
+resource "azurerm_key_vault_access_policy" "key_vault_consumer_function_access_policy" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_function_app.order_function_consumer.identity[0].principal_id
+
+  secret_permissions = ["delete", "get", "set", "list"]
+}
+
