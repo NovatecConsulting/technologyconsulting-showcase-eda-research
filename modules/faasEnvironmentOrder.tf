828752ab40c4df05de052c8d5b198a79b4d2a6c1
Tobias Bohn <tobiasbohnxz@gmail.com>
2020-12-09 15:41:56 +0100

    linux app service plan
    kind=linux

--- modules/faasEnvironmentOrder.tf
+++ modules/faasEnvironmentOrder.tf
@@ -1,20 +1,20 @@
-#resource "azurerm_app_service_plan" "function_app_service_plan" {
-#    name = "tc-eda-iac-${var.environment}-function-app-service-plan"
-#    location = azurerm_resource_group.resourceGroup.location
-#    resource_group_name = azurerm_resource_group.resourceGroup.name
-#    kind = "Windows"
-#
-#    sku {
-#        tier = "Free"
-#        size = "F1"
-#    }
-#}
+resource "azurerm_app_service_plan" "function_app_service_plan_linux" {
+   name = "tc-eda-iac-${var.environment}-function-app-service-plan-linux"
+   location = azurerm_resource_group.resourceGroup.location
+   resource_group_name = azurerm_resource_group.resourceGroup.name
+   kind = "Linux"
+
+   sku {
+       tier = "Free"
+       size = "F1"
+   }
+}
 
 resource "azurerm_function_app" "order_function_producer" {
     name = "tc-eda-iac-${var.environment}-function-order-producer"
     location = azurerm_resource_group.resourceGroup.location
     resource_group_name = azurerm_resource_group.resourceGroup.name
-    app_service_plan_id = azurerm_app_service_plan.function_app_service_plan.id
+    app_service_plan_id = azurerm_app_service_plan.function_app_service_plan_linux.id
     storage_account_name = azurerm_storage_account.storage_account.name
     storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
     version = "~3"
@@ -38,7 +38,7 @@ resource "azurerm_function_app" "order_function_consumer" {
     name = "tc-eda-iac-${var.environment}-function-order-consumer"
     location = azurerm_resource_group.resourceGroup.location
     resource_group_name = azurerm_resource_group.resourceGroup.name
-    app_service_plan_id = azurerm_app_service_plan.function_app_service_plan.id
+    app_service_plan_id = azurerm_app_service_plan.function_app_service_plan_linux.id
     storage_account_name = azurerm_storage_account.storage_account.name
     storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
     version = "~3"
